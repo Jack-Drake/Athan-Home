80a0d94a1dfa6a258ac06d76be987771c5080dc8
yahyaisfast <44513448+yahyaisfast@users.noreply.github.com>
2019-03-19 12:46:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,74 @@
-//read the json file
-var fs = require("fs");
-var content = fs.readFileSync("salah.json");
+//requires the request module
+var request = require('request');
+var json;
 
-//parses the json file
-var json = JSON.parse(content);
+request('link to json file to be parsed', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+     json = JSON.parse(body);
 
-//console.log(date.getMinutes());
-//console.log(json.fajr);
-checkSalah();
-
-//checks to see every second
-setInterval(checkSalah, 1000);
+     //must be inside the if statement otherwise the json code wont work
+     setInterval(makeathan, 60000);
+  }
+})
 
 function makeathan(){
-
-        const GoogleHome = require("google-home-push");
-
-        // Pass the name or IP address of your device
-        const myHome = new GoogleHome("192.168.0.13");
-        const myHome1 = new GoogleHome("192.168.0.17");
-
-        myHome.push("http://www.islamcan.com/audio/adhan/azan9.mp3");
-        myHome1.push("http://www.islamcan.com/audio/adhan/azan9.mp3");
+                const GoogleHome = require("google-home-push");
+                
+                //creates a new instance of the googlehome
+                const myHome = new GoogleHome("ip-address");
+        
+                //pushes the mp3 music out
+                myHome.push("mp3 file to be outputted to the speaker");
 }
 
 function checkSalah(){
 
         //makes a date object
         var date = new Date();
+        var compare;
 
+        //checks to accompany for the loss of a number in the return value of the date object
+        if(date.getHours() < 10)
+        {
+                var compare = "0" + date.getHours() + ":";
+        }
+        else
+        {
+                var compare = date.getHours() + ":";
+        }
+
+        if(date.getMinutes() < 10)
+        {
+                compare += "0" + date.getMinutes();
+        }
+        else
+        {
+                compare += date.getMinutes();
+        }
         //always include the seconds otherwise it will be recalling for 1 minute straight
-        var compare = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-
-
-        //console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
 
         //checks the json with the current time
-        if(compare == json.fajr)
+        if(compare == json.data.timings.Fajr)
         {
                 console.log("It's fajr time!");
                 makeathan();
         }
-        else if(compare == json.thuhr)
+        else if(compare == json.data.timings.Dhuhr)
         {
                 console.log("It's thuhr time!");
                 makeathan();
         }
-        else if(compare == json.asr)
+        else if(compare == json.data.timings.Asr)
         {
                 console.log("It's asr time!");
                 makeathan();
         }
-        else if(compare == json.maghrib)
+        else if(compare == json.data.timings.Maghrib)
         {
                 console.log("It's maghrib time!");
                 makeathan();
         }
-        else if(compare == json.isha)
+        else if(compare == json.data.timings.Isha)
         {
                 console.log("It's ishaa time!");
                 makeathan();
