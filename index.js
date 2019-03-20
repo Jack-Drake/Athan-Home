8bc28dec98b1752cf976364b55f1c0a4e56ba1d5
Gilgamesh <32211128+Dontmindmes@users.noreply.github.com>
2019-03-19 17:38:07 -0700

    fixed formating
    fixed formating

--- index.js
+++ index.js
@@ -16,60 +16,50 @@ function makeathan(){
                 
                 //creates a new instance of the googlehome
                 const myHome = new GoogleHome("ip-address");
-        
+  
                 //pushes the mp3 music out
                 myHome.push("mp3 file to be outputted to the speaker");
 }
 
 function checkSalah(){
-
         //makes a date object
         var date = new Date();
         var compare;
 
         //checks to accompany for the loss of a number in the return value of the date object
-        if(date.getHours() < 10)
-        {
+        if(date.getHours() < 10) {
                 var compare = "0" + date.getHours() + ":";
         }
-        else
-        {
+        else {
                 var compare = date.getHours() + ":";
         }
 
-        if(date.getMinutes() < 10)
-        {
+        if(date.getMinutes() < 10) {
                 compare += "0" + date.getMinutes();
         }
-        else
-        {
+        else {
                 compare += date.getMinutes();
         }
         //always include the seconds otherwise it will be recalling for 1 minute straight
 
         //checks the json with the current time
-        if(compare == json.data.timings.Fajr)
-        {
+        if(compare == json.data.timings.Fajr) {
                 console.log("It's fajr time!");
                 makeathan();
         }
-        else if(compare == json.data.timings.Dhuhr)
-        {
+        else if(compare == json.data.timings.Dhuhr) {
                 console.log("It's thuhr time!");
                 makeathan();
         }
-        else if(compare == json.data.timings.Asr)
-        {
+        else if(compare == json.data.timings.Asr) {
                 console.log("It's asr time!");
                 makeathan();
         }
-        else if(compare == json.data.timings.Maghrib)
-        {
+        else if(compare == json.data.timings.Maghrib) {
                 console.log("It's maghrib time!");
                 makeathan();
         }
-        else if(compare == json.data.timings.Isha)
-        {
+        else if(compare == json.data.timings.Isha) {
                 console.log("It's ishaa time!");
                 makeathan();
         }
